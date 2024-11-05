8d004d18d363e2086069467d9c89d95a4d18673d
Illian Yamandú <yamandu.developer@gmail.com>
2024-11-05 08:04:27 -0300

    wip

---
+++ resources/views/livewire/chat.blade.php
@@ -0,0 +1,8 @@
+<div class="grid grid-rows-3 grid-flow-col gap4">
+    <div class="row-span-3 bg-slate-700 p-4 rounded-3xl">
+        01
+    </div>
+    <div class="col-span-2 row-span-3">
+        02
+    </div>
+</div>
